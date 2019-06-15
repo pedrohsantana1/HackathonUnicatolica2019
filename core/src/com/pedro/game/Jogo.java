651b49e0428631c751367d949b4e3cb0d4b8086d
pedrohsantana1 <pedro.amaral@a.catolica-to.edu.br>
2019-06-15 03:54:21 -0300

    Segundo commit projeto

--- core/src/com/pedro/game/Jogo.java
+++ core/src/com/pedro/game/Jogo.java
@@ -2,32 +2,110 @@ package com.pedro.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Jogo extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	//Texturas
+	private SpriteBatch batch;
+	private Texture[] fundo;
+	private Texture[] personagem;
+	private Texture pequi;
+
+
+	//Atributos de configuração
+	private float larguraDispositivo;
+	private float alturaDispositivo;
+	private float gravidade = 0;
+	private float posicaoInicialVerticalPersonagem;
+	private float posicaoTela = 0;
+
 	@Override
 	public void create () {
+		inicializarObjetos();
+		inicializarTexturas();
+
+
+
+	}
+	private void inicializarObjetos(){
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		larguraDispositivo = Gdx.graphics.getWidth();
+		alturaDispositivo = Gdx.graphics.getHeight();
+		posicaoInicialVerticalPersonagem = alturaDispositivo/2 - alturaDispositivo/4;
+		posicaoTela = larguraDispositivo;
+	}
+
+	private void inicializarTexturas(){
+		fundo = new Texture[3];
+		fundo[0] = new Texture("fundo1.png");
+		fundo[1] = new Texture("fundo2.png");
+		fundo[2] = new Texture("fundo3.png");
+		personagem = new Texture[3];
+		personagem[0] = new Texture("personagem1.png");
+		personagem[1] = new Texture("personagem2m1.png");
+		personagem[2] = new Texture("personagem2m2.png");
+		pequi = new Texture("pequi.png");
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		verificarEstadoJogo();
 	}
-	
+
+	private void verificarEstadoJogo(){
+		batch.begin();
+
+		boolean toqueTela = Gdx.input.justTouched();
+
+		if( toqueTela ){
+			gravidade = 25;
+
+		}
+
+		if(posicaoInicialVerticalPersonagem > 0 || toqueTela ) {
+			if(posicaoInicialVerticalPersonagem + gravidade <=0)
+			{
+				posicaoInicialVerticalPersonagem = 0;
+			}
+			else {
+				posicaoInicialVerticalPersonagem = posicaoInicialVerticalPersonagem + gravidade;
+			}
+		}
+		desenharTexturas();
+
+		gravidade--;
+
+		movimentarFundo();
+
+		batch.end();
+
+	}
+	private void movimentarFundo(){
+		//Movimentar fundo
+		posicaoTela = posicaoTela - Gdx.graphics.getDeltaTime()*200;
+		if(posicaoTela <= 0){
+			posicaoTela = larguraDispositivo;
+		}
+	}
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
+
+
+	private void desenharTexturas(){
+		batch.draw(fundo[1],posicaoTela,0, larguraDispositivo, alturaDispositivo);
+		batch.draw(fundo[1],posicaoTela-larguraDispositivo,0, larguraDispositivo, alturaDispositivo);
+		batch.draw(personagem[0], 0, posicaoInicialVerticalPersonagem);
+		batch.draw(pequi, posicaoTela, 300);
+
+
+
+	}
+
+
+
 }
